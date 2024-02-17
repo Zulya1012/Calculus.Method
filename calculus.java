deb186c321ebbea6dceb14dfcfb57321a369f8d3
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-17 17:17:46 +0400

    Update calculus.java

--- calculus.java
+++ calculus.java
@@ -6,38 +6,36 @@ public class calculus {
     int a = sc.nextInt();
     valueEnter(sc);
     int b = sc.nextInt();
-  
+
     Scanner in = new Scanner(System.in);
-    
+
     selectOperation(in);
-    
+
     String x = in.nextLine();
-      
+
     conditionSimvol(x, a, b);
-    
-    
-    }
 
+  }
 
-  public static void enterValue(Scanner sc){
+  public static void enterValue(Scanner sc) {
     System.out.println("Enter a value:");
-      
+
   }
-  public static void valueEnter(Scanner sc){
-     System.out.println("Enter a value:");
+  public static void valueEnter(Scanner sc) {
+    System.out.println("Enter a value:");
   }
-  public static void selectOperation(Scanner in){
+  public static void selectOperation(Scanner in ) {
     System.out.println("Select an operation:");
     System.out.println(" +");
     System.out.println(" -");
     System.out.println(" *");
     System.out.println(" /");
   }
-    
-  public static void conditionSimvol(String x, int a, int b){
-    
-      if (x.equals("+")) {
-        System.out.println(a + b);
+
+  public static void conditionSimvol(String x, int a, int b) {
+
+    if (x.equals("+")) {
+      System.out.println(a + b);
     } else if (x.equals("-")) {
       System.out.println(a - b);
     } else if (x.equals("*")) {
@@ -45,8 +43,8 @@ public class calculus {
     } else if (x.equals("/")) {
 
       System.out.println(a / b);
+    }
+
   }
-  
-  }
-  
+
 }
